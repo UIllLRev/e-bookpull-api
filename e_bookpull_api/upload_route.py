8911468e6ed2da47113569ba930518d0e87896e0
Matt Loar <matt@loar.name>
2018-04-25 18:59:05 -0500

    Return computed path for uploads.
    Also check if work id is valid.

--- e_bookpull_api/upload_route.py
+++ e_bookpull_api/upload_route.py
@@ -22,12 +22,18 @@ class UploadRoute:
         def upload(work_id):
             if 'file' in request.files:
                 work = Work.query.get(work_id)
-                f = request.files['file']
-                target_dir = os.path.join(upload_path, work.author_name)
-                mkdir_p(target_dir)
-                filename = os.path.join(target_dir, datetime.now().strftime("%Y-%m-%d_%H-%M-%S_") + secure_filename(f.filename))
-                f.save(filename)
-                return json.dumps({"url": "/members/bookpull/" + os.path.relpath(filename)}), 200, {"Content-Type": "application/vnd.api+json"}
+                if work:
+                    f = request.files['file']
+                    target_dir = os.path.join(upload_path, work.author_name)
+                    root = os.path.commonprefix([os.path.abspath(target_dir), os.path.abspath(request.environ['SCRIPT_NAME'])])
+                    mkdir_p(target_dir)
+                    filename = os.path.join(target_dir, datetime.now().strftime("%Y-%m-%d_%H-%M-%S_") + secure_filename(f.filename))
+                    f.save(filename)
+                    return json.dumps({"url": os.path.relpath(filename, root)}), 200, {"Content-Type": "application/vnd.api+json"}
+                return json.dumps({
+                    "errors": [{"status": "400", "detail": "Invalid work id.", "title": "Bad request"}],
+                    "jsonapi": {"version": "1.0"}
+                    }), 400, {"Content-Type": "application/vnd.api+json"}
             return json.dumps({
                 "errors": [{"status": "400", "detail": "No file named 'file' in request.", "title": "Bad request"}],
                 "jsonapi": {"version": "1.0"}
